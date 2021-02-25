16d919637490c0b76297bff5e9524c99a0f84256
swetabaliarsingh <79101851+swetabaliarsingh@users.noreply.github.com>
2021-02-25 16:09:05 +0530

    Update and rename README.md to r-class-1.R

---
+++ r-class-1.R
@@ -0,0 +1,40 @@
+# r-classwork-1
+print ("hello world")
+[1] "hello world"
+> 2+3
+[1] 5
+> 2-3
+[1] -1
+> 2*3
+[1] 6
+> 2/4
+[1] 0.5
+> y= 2+3
+> y
+[1] 5
+> nummy<-c(2,3,4)
+> nummy_int<-c(1L,2L,3L)
+> typeof(nummy)
+[1] "double"
+> typeof(nummy_int)
+[1] "integer"
+> is.numeric(nummy)
+[1] TRUE
+> is.numeric(nummy_int)
+[1] TRUE
+> is.double(nummy)
+[1] TRUE
+> length(nummy)
+[1] 3
+> days <- c("Mon","Tue","Wed")
+> days
+[1] "Mon" "Tue" "Wed"
+> week_end <- c("Sat","Sun")
+> more_days <- c(days,"Thu","Fri",week_end)
+> more_days
+[1] "Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun"
+> types <- c("int","double","character")
+> types
+[1] "int"       "double"    "character"
+> typeof(types)
+[1] "character"
